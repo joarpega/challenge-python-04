6d8dcd8af7b6cba0aa1fba25a08c9b6640e68fcd
Jorge A. Pérez Garay <jperez@sw1848.com>
2020-08-11 02:36:22 -0500

    Completed exercise decorators

--- html_decorators.py
+++ html_decorators.py
@@ -1,22 +1,32 @@
 def div(func):
     # You have to code here!
-    pass
+    def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        return f'<div>{result}</div>'
+    
+    return wrapper
 
 
 def article(func):
     # You have to code here!
-    pass
-
+    def wrapper(*args, **kwargs):
+        return f'<article>{func(*args, **kwargs)}</article>'
+    
+    return wrapper
+        
 
 def p(func):
     # You have to code here!
-    pass
+    def wrapper(*args, **kwargs):
+        return f'<p>{func(*args, **kwargs)}</p>'
+    return wrapper
 
 
 # Here you must apply the decorators, uncomment this later
 # @div
 # @article
 # @p
+@p
 def saludo(nombre):
     return f'¡Hola {nombre}, ¿Cómo estás?'
 
